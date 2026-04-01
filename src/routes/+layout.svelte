b8474e71b15d069bf986554a01795c20226160d4
LORN-Hongnida <137864918+LORN-Hongnida@users.noreply.github.com>
2026-04-02 01:58:20 +0700

    #14 create-news-feed-page-design

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
+	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import Navbar from '$lib/component/Navbar.svelte';
+	import Sidebar from '$lib/component/Sidebar.svelte';
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
-{@render children()}
+<Navbar />
+<div class="app-shell">
+	<Sidebar />
+	<main class="page-content">{@render children()}</main>
+</div>
