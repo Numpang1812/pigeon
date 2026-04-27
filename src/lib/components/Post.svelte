03b1da995337f3853da54cccd0c39ac066183801
SunKhingleak <2024028sun@aupp.edu.kh>
2026-04-27 15:27:27 +0700

    change

--- src/lib/components/Post.svelte
+++ src/lib/components/Post.svelte
@@ -751,11 +751,7 @@
 					{#if props.avatar_url}
 						<img class="avatar-image" src={props.avatar_url} alt={`${props.post_tag} tag icon`} />
 					{:else}
-						<div class="avatar-placeholder" aria-hidden="true">
-							<svg viewBox="0 0 24 24" fill="currentColor">
-								<path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
-							</svg>
-						</div>
+							<img class="avatar-image" src="/default-avatar.svg" alt={`${props.author_name} default avatar`} />
 					{/if}
 				</a>
 			{:else}
@@ -763,11 +759,7 @@
 					{#if props.avatar_url}
 						<img class="avatar-image" src={props.avatar_url} alt={`${props.post_tag} tag icon`} />
 					{:else}
-						<div class="avatar-placeholder" aria-hidden="true">
-							<svg viewBox="0 0 24 24" fill="currentColor">
-								<path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
-							</svg>
-						</div>
+							<img class="avatar-image" src="/default-avatar.svg" alt={`${props.author_name} default avatar`} />
 					{/if}
 				</div>
 			{/if}
@@ -1175,11 +1167,6 @@
 		display: flex;
 		align-items: center;
 		justify-content: center;
-	}
-
-	.avatar-placeholder svg {
-		width: 26px;
-		height: 26px;
 	}
 
 	.author-info {
